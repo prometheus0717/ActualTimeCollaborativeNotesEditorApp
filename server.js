b034c8919b556ea7c191d6eba195da25f5c5f31b
D Kumar <deepkumar0501@gmail.com>
2025-09-27 10:09:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const WebSocket = require('ws');         
 const cors = require('cors');               
 const app = express();                         
-app.use(cors());                          
+app.use(cors());                           
 
 const server = http.createServer(app);              
 const wss = new WebSocket.Server({ server });       
