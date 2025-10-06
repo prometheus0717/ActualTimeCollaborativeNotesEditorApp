63ba82c4e42ec9c22f4c00160eb8c23f566a2a00
D Kumar <deepkumar0501@gmail.com>
2025-10-06 22:15:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });             
         
 let document = ""; // Store the document content         
-      
+       
 wss.on('connection', (ws) => {  
     console.log('New client connected');    
  
